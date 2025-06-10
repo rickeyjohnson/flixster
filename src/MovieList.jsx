94d6678904e2b3eddcb802b9db90fe749964c6fd
rickey johnson <rdjwinter2004@gmail.com>
2025-06-09 17:21:52 -0700

    fetched movie data from API and successfully parsed and loaded data onto movie list

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -5,7 +5,32 @@ import MovieCard from "./MovieCard"
 import './MovieList.css'
 
 const MovieList = () => {
-    const movies = parseMovieData(data)
+    const [movies, setMovies] = useState(null)
+
+    useEffect(() => {
+        async function fetchData() {
+            const options = {
+                'method': 'GET',
+                'headers': {
+                    'accept': 'application/json',
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTJhNGI5ZDFlODAzYjI4MTRkYjRkNTY1NmMwMzQzYyIsIm5iZiI6MTc0OTUwNTIzMi4zNDQsInN1YiI6IjY4NDc1NGQwNzg5ZWY1MTA5MzFlYWQwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BMZp9EdAO53TTVNUtT06TtxGQmI_22hJWc7luUKDU1s'
+                }
+            }
+
+            try {
+                const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+                const data = await response.json()
+                
+                setMovies(parseMovieData(data))
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchData()
+
+    }, [])
+
     console.log(movies)
 
     return (
